6cdd0ecf5c1756007f15f4de4bd14393c439aff6
Patriot <prateek.sscet@gmail.com>
2022-03-07 15:48:35 -0600

    Update mediaOrganizer.sh
    Added fallback to probe file name to get creation time if ExifTool data is missing.

--- mediaOrganizer.sh
+++ mediaOrganizer.sh
@@ -98,8 +98,8 @@ media_list=$(echo "$find_command" |bash)
 #media_list=$(find "${input_dir}" -path "./$(basename "$output_dir")"  -prune -o -type f   \( -iname "*.png" -o -iname "*.3gp" -o -iname "*.MOV" -o -iname "*.m4v" -o -iname "*.avi" -o -iname "*.gif" -o -iname "*.jpeg" -o -iname "*.jpg" -o -iname "*.mkv" -o -iname "*.mpg" -o -iname "*.bmp" -o -iname "*.flv" -o -iname "*.wmv" -o -iname "*.mp4" \))
 
 total_files=$(echo "$media_list" |grep -E . |wc -l)
-if [ ${total_files:-0} -eq 0 ];then
-        echo "$(date): [Info]: No media file found ${media_list}. Supported format by this tool are:  $(echo $find_command  |grep -oP 'name\K\s+[^ ]+'|tr '\n' ', ') "
+if [ "${total_files:-0}" -eq 0 ];then
+        echo "$(date): [Info]: No media file found ${media_list}. Supported format by this tool are:  $(echo "$find_command"  |grep -oP 'name\K\s+[^ ]+'|tr '\n' ', ') "
         exit 69
 else
         initial_size_input_dir=$(du -sh "${input_dir}" |awk '{print $1}')
@@ -110,27 +110,33 @@ count=1
 echo "$media_list"|while read -r file;
 do
         if [ -d "$file" ];then
-                echo "$(date): [Warn]: Skipping directory $file"
+                echo "$(date): [Warning]: Skipping directory $file"
                 continue;
         fi
         echo "$(date): [Info]: Processing $count/$total_files file. Current file is $(basename "$file")"
         count=$((count+1))
         out_path=$(exiftool  "$file" |awk -v format="${format}" -v OFS='/' '/^Create Date/{split($4,a,":");if(format == "YYYYMMDD") {print a[1],a[2],a[3]}; if(format == "YYYYMM") {print a[1],a[2]} if(format == "YYYY") {print a[1]} ;exit}')
-        if [ -z "${out_path}" ];then
-                out_path=$(exiftool  "$file" | awk -v format="${format}" -v OFS='/' '/^File Modification Date/{split($5,a,":");if(format == "YYYYMMDD") {print a[1],a[2],a[3]}; if(format == "YYYYMM") {print a[1],a[2]} if(format == "YYYY") {print a[1]};exit}')
+        if [[ $format == "YYYY" && $out_path == "0000"  ||  $format == "YYYYMM" && $out_path == "0000/00" ||  $format == "YYYYMMDD" &&  $out_path == "0000/00/00" || -z $out_path ]];then
+                echo "$(date): [Warning]: Unable to get the creation time of $file using exiftool, falling back to probe file name to get a valid date"
+                date_from_filename=$(echo "$file"|grep -oP '20\d{2}-*([0][1-9]|[1][012])-*([0][1-9]|[1][0-9]|[2][0-9]|[3][0-1])'||true)
+                if ! date -d "$date_from_filename" &>/dev/null;then
+                        echo "$(date): [Warning]: Unable to obtain a valid date from the filename probe, moving file to ${output_dir}/unknown_origin"
+
+                        if [ ! -d "${output_dir}/unknown_origin" ];then
+                              if ! mkdir -p "${output_dir}"/unknown_origin 2>/dev/null;then
+                                        echo "$(date): [Error]: Unable to create ${output_dir}/unknown_origin"
+                                        exit 1
+                              fi
+                        fi
+
+                        mv "${file}" "${output_dir}/unknown_origin/"
+                        continue;
+                else
+
+                        out_path=$(date -d "$date_from_filename" +%F |awk -v format="${format}" -v OFS='/' '{split($1,a,"-");if(format == "YYYYMMDD") {print a[1],a[2],a[3]}; if(format == "YYYYMM") {print a[1],a[2]} if(format == "YYYY") {print a[1]} ;exit}')
+                fi
         fi
 
-        if [ -z "${out_path}" ];then
-                echo "$(date): [Warn]: Unable to fetch the create time of $(basename "$file"), moving to ${output_dir}/unknown_origin"
-                if [ ! -d "${output_dir}/unknown_origin" ];then
-                        if ! mkdir -p "${output_dir}"/unknown_origin 2>/dev/null;then
-                                echo "$(date): [Error]: Unable to create ${output_dir}/unknown_origin"
-                                exit 1
-                        fi
-                fi
-                mv "${file}" "${output_dir}/unknown_origin/"
-                continue;
-        fi
         if [ ! -d "${output_dir}/${out_path}" ];then
                 if ! mkdir -p "${output_dir}/${out_path}" ;then
                         echo "$(date): [Error]: Unable to create ${output_dir}/${out_path} directory."
